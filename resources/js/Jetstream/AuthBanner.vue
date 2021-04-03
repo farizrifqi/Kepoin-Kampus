2942e46dfe46c98d5715979b99a494df0cb727ed
Fariz Rifqi <farizrifqi26@gmail.com>
2021-04-03 23:40:07 +0800

    first

---
+++ resources/js/Jetstream/AuthBanner.vue
@@ -0,0 +1,10 @@
+<template>
+<img class="self-center" src="/image/logo-cream.png" width="75">
+</template>
+<script>
+    export default {
+        data:() => {
+
+        }
+    }
+</script>
---
+++ resources/js/Jetstream/AuthenticationCardLogo.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex justify-left ">
+        <div><img class="self-center" src="/image/logo-cream.png" width="55"> </div>
+        <div class="self-center"><p class="ml-3 font-sans md:roboto text-2xl font-bold" style="color: #FBE99E">Kepoin Kampus</p></div>
+    </div>
+</template>
